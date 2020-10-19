39df0df79215f534e26b2eead8bd46bc8db48fa2
guivipom <guivipom@gmail.com>
2020-10-19 16:17:25 +0200

    Add basic endpoint testing

--- test/note.test.js
+++ test/note.test.js
@@ -24,3 +24,23 @@ test('should create a new note', async ()=> {
             tags: ['tag 1', 'tag 2', 'tag 3']})
     .expect(201)
 })
+
+test('should NOT create a new note', async ()=> {
+    await request(app).post('/notes').send({
+            content: 'New note content ',
+            tags: ['tag 1', 'tag 2', 'tag 3']
+    }).expect(400)
+})
+
+test('Should get note by id', async () => {
+    await request(app)
+        .get('/notes/'+noteOneId)
+        .expect(200)
+})
+
+
+test('Should delete note by id', async () => {
+    await request(app)
+        .delete('/notes/'+noteOneId)
+        .expect(200)
+})
